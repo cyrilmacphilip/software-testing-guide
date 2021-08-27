339ca9718c64862a8ae8cf1048940268402f740e
Cyril Mac Philip <cyrilmacphilip@gmail.com>
2021-08-28 00:52:26 +0530

    use lombok

--- src/main/java/com/cmp/guide/view/Student.java
+++ src/main/java/com/cmp/guide/view/Student.java
@@ -1,50 +1,12 @@
 package com.cmp.guide.view;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
-import java.util.Objects;
-
+@Data
 public class Student {
     @JsonProperty("id")
     Integer id;
     @JsonProperty("name")
     String name;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return Objects.equals(id, student.id) && Objects.equals(name, student.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    public Student() {
-        //default
-    }
-
-    public Student(Integer id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 }
